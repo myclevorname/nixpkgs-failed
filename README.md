70c2cde6d7aa04b673cf106345f1e5a22369a154
Sam Connelly <myclevorname@gmail.com>
2024-10-06 15:19:43 -0400

    Proof-of-concept

---
+++ README.md
@@ -0,0 +1,32 @@
+# nixpkgs-failed
+
+This program gets a random failing package from [nixos:trunk-combined](https://hydra.nixos.org/jobset/nixos/trunk-combined/evals).
+It is currently a work-in-progress.
+
+## Build Instructions
+
+The official method is to run:
+```console
+$ nix build
+```
+but you can also run:
+```bash
+$ cargo build
+```
+
+## Issues
+
+Q: Why is this so slow?
+
+A: I am calling the Hydra API directly.
+I can't get the latest evaluation unless I get the list of every evaluation, and I have to manually check each build to see if it failed.
+I could use the HTML page, but that isn't as simple as calling an API and decoding JSON.
+
+
+## TODO
+
+[X] Get the latest evaluation
+[X] Get a failing build
+[ ] Print metadata about it
+[ ] Make it pretty
+[ ] CLI options
---
+++ src/main.rs
@@ -0,0 +1,89 @@
+use reqwest::blocking::Client;
+use serde::de::DeserializeOwned;
+use serde::Deserialize;
+use std::time::Duration;
+
+// Change these!
+const HYDRA_URL: &str = "https://hydra.nixos.org";
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct JobsetEvals {
+    pub evals: Box<[Evaluation]>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Evaluation {
+    pub builds: Box<[u64]>,
+    pub id: u64,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Build {
+//    id: u64,
+    buildstatus: u64,
+//    finished: u8,
+    nixname: String
+}
+
+fn get_url<T>(url: &str) -> Result<T, reqwest::Error>
+where
+    T: DeserializeOwned,
+{
+    Client::new()
+        .get(format!("{HYDRA_URL}/{url}"))
+        .header(reqwest::header::CONTENT_TYPE, "application/json")
+        .timeout(Duration::new(240, 0))
+        .send()?
+        .json::<T>()
+}
+
+// Should cache this in a file, then make a CLI argument that manually updates it
+fn get_latest_evaluation(
+    project_name: &str,
+    jobset_name: &str,
+) -> Result<Evaluation, reqwest::Error> {
+    let mut evals = get_url::<JobsetEvals>(&format!("/jobset/{project_name}/{jobset_name}/evals"))?.evals;
+    evals.sort_by_key(|x| x.id);
+    Ok(evals.last().unwrap_or_else(|| panic!(
+                "The latest evaluation of jobset {}:{} somehow had no builds. If you see this, let me know.",
+                project_name,
+                jobset_name)).clone())
+}
+
+// See https://github.com/NixOS/hydra/blob/95003f2eb503f71979856470c7caea946f1ae7f0/src/hydra-queue-runner/state.hh#L53
+fn is_failing_build(build: &Build) -> bool {
+    match build.buildstatus {
+        0 => false,     // bsSuccess
+        1 => true,      // bsFailed
+        2 => true,      // bsDepFailed
+        3 => true,      // bsAborted
+        4 => false,     // bsCancelled
+        6 => true,      // bsFailedWithOutput
+        7 => true,      // bsTimedOut
+        8 => true,      // bsCachedFailure
+        9 => true,      // bsUnsupported
+        10 => true,     // bsLogLimitExceeded
+        11 => true,     // bsNarSizeLimitExceeded
+        12 => true,     // bsNotDeterministic
+        100 => false,   // bsBusy
+        _ => panic!("Unknown build status found")
+    }
+}
+
+/// This does not give the error
+fn get_failing_builds(evaluation: &Evaluation) -> impl Iterator<Item = Build> + '_ {
+    evaluation.builds
+        .iter()
+        .flat_map(|x| get_url::<Build>(&format!("/build/{x}")))
+        .filter(is_failing_build)
+}
+
+
+
+
+fn main() {
+    let evaluation =
+        get_latest_evaluation("nixpkgs", "trunk").unwrap();
+    let failing_build = get_failing_builds(&evaluation).next().unwrap();
+    println!("{:#?} has failed", failing_build.nixname);
+}
